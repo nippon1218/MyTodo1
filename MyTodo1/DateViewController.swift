fc2fe9c2427d4f459c3a2b5973d3a615aa35f4dc
VincentChen <nippon1218@163.com>
2019-06-30 15:46:47 +0800

    修改了第一额view的UI

--- MyTodo1/DateViewController.swift
+++ MyTodo1/DateViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class DateViewController: UIViewController {
 
+    @IBOutlet weak var childButton: UIButton!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var shoppingButton: UIButton!
+    @IBOutlet weak var travelButton: UIButton!
+    @IBOutlet weak var todoItemLabel: UITextField!
+    @IBOutlet weak var todoDatePicker: UIDatePicker!
+    
+    var todo: TodoItem?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +27,62 @@ class DateViewController: UIViewController {
     }
     
 
+    @IBAction func selectChild(_ sender: Any) {
+        resetButton()
+        phoneButton.isSelected = true
+    }
+    
+    @IBAction func selectPhone(_ sender: Any) {
+        resetButton()
+        phoneButton.isSelected = true
+    }
+    
+    @IBAction func selectShopping(_ sender: Any) {
+        resetButton()
+        shoppingButton.isSelected = true
+    }
+
+    @IBAction func selectTravel(_ sender: Any) {
+        resetButton()
+        travelButton.isSelected = true
+    }
+    
+    func resetButton() {
+        childButton.isSelected = false
+        phoneButton.isSelected = false
+        shoppingButton.isSelected = false
+        travelButton.isSelected = false
+    }
+    
+    @IBAction func tapDone(_ sender: Any) {
+        var image = ""
+        if childButton.isSelected {
+            image = "child-selected"
+        }
+        else if phoneButton.isSelected {
+            image = "phone-selected"
+        }
+        else if shoppingButton.isSelected {
+            image = "shopping-cart-selected"
+        }
+        else if travelButton.isSelected {
+            image = "travel-selected"
+        }
+        
+        if let todo = todo {
+            todo.image = image
+            todo.title = todoItemLabel.text!
+            todo.date = todoDatePicker.date
+        } else {
+            let uuid = UUID().uuidString
+            todo = TodoItem(id: uuid, image: image, title: todoItemLabel.text!, date: todoDatePicker.date)
+           // todos.append(todo!)
+        }
+        
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- MyTodo1/ViewController.swift
+++ MyTodo1/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+var todos: [TodoItem] = []
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
